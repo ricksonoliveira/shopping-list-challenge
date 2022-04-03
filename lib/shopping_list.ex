925b9141e327e37404c1dce6729e82d28382bf9c
ricksonolveira <ricksonoliveira>
2022-04-03 15:06:33 -0300

    feat: app init

---
+++ lib/shopping_list.ex
@@ -0,0 +1,18 @@
+defmodule ShoppingList do
+  @moduledoc """
+  Documentation for `ShoppingList`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ShoppingList.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
