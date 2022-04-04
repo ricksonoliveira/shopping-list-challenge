1a5b56e9a39fa63849efed5b53d4c1522e9c014e
ricksonoliveira <riksongleison471@gmail.com>
2022-04-04 14:45:34 -0300

    feat: finishing distribution of values to buyers

--- lib/emails_list.ex
+++ lib/emails_list.ex
@@ -6,23 +6,36 @@ defmodule EmailsList do
   defstruct email: nil
 
   def distrubute_to_buyers(buyers, sum) do
-    value_to_distribute = sum / Enum.count(buyers)
+    buyers_count = Enum.count(buyers)
+    value_to_distribute = Float.to_string(sum / buyers_count)
 
-    if is_float(value_to_distribute) do
-      total = value_to_distribute * sum
-      rest = sum - total
+    [value_splitted, rest_splitted] = value_to_distribute |> String.split(".")
+    value_integer_to_distribute = String.to_integer(value_splitted)
 
-      [_, value_splitted] =
-        value_to_distribute
-          |> Float.to_string()
-          |> String.split(".")
-      ceil_length = String.length(value_splitted)
-      ^value_to_distribute = Float.ceil(value_to_distribute, ceil_length)
-    end
+    rest_value_to_distribute = get_rest_of_value_to_distribute(value_splitted, rest_splitted, buyers_count, sum)
+    IO.inspect(rest_value_to_distribute)
+    IO.inspect(value_integer_to_distribute)
 
-    buyers
-      |> Enum.map(fn buyer ->
-        ["#{buyer.email}": value_to_distribute]
+    emails_distributed = %{}
+    {emails_list, _} = buyers
+      |> Enum.map_reduce(1, fn(buyer, acc) ->
+        if String.to_integer(rest_splitted) !== 0 and buyers_count == acc do
+          {Map.put(emails_distributed, buyer.email, value_integer_to_distribute + rest_value_to_distribute), acc}
+        else
+          {Map.put(emails_distributed, buyer.email, value_integer_to_distribute), acc + 1}
+        end
       end)
+
+      emails_list
+  end
+
+  defp get_rest_of_value_to_distribute(value_splitted, rest_splitted, buyers_count, sum) do
+    case String.to_integer(rest_splitted) do
+      0 ->
+        0
+      _ ->
+        total = String.to_integer(value_splitted) * buyers_count
+        sum - total
+    end
   end
 end
